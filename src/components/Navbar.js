6f2fbe53399f0fff3bdf963acf6cabadfcebe8da
MBushari77 <bigmbushari@gmail.com>
2022-04-29 15:38:40 +0200

    another commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,36 @@
-
+import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../static/logo.svg';
+import { useState } from 'react';
 
 
 const Navbar = () => {
 
+  const [navSt, setNavSt] = useState(false);
+  const [buttonIcon, setButtonIcon] = useState('icon-menu')
 
+  const toggleNav = () => {
+    setNavSt(!navSt);
+    buttonIcon === 'icon-menu' ? setButtonIcon('icon-cancel') : setButtonIcon('icon-menu');
+  }
 
   return (
-    <nav className='row'>
-      <div className='col-md-3 col-sm-6'>
-        <div className='nav_logo'>
-          <Link to='/' className='nll'>
-            <img src={logo} />
-          </Link>
-        </div>
-
+    <nav className="navbar-expand-lg navbar-light navbar">
+      <div className='nav_logo'>
+        <Link to='/' className='nll'> 
+          <img src={logo} />
+        </Link>
       </div>
-      <div className='col-md-6 col-sm-6 nav_buttons_container'>
-        <div className='nav_actions'>
-          {/* <Link className="navbar-button" to="/">Home</Link> */}
+      <button onClick={ toggleNav } className="navbar-toggler" type="button" >
+        <i className={ buttonIcon }></i>
+      </button>
+      <div className={navSt ? "collapse navbar-collapse right_alignv": "collapse navbar-collapse right_alignv mobile_nav"} id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+            <Link className='navbar_button' to='/session'>Book a session</Link>
+          <div className='col-md-3 nav_contact_us_container'>
+              <Link className='navbar_button  navbar_button_contact_us' to='/contact' >CONTACT US</Link>
+          </div>
         </div>
-      </div>
-      <div className='col-sm-12 col-md-3 nav_contact_us_container'>
-          <Link className='navbar_button  navbar_button_contact_us' to='/contact' >CONTACT US</Link>
       </div>
     </nav>
   )
--- src/components/OurGallary.js
+++ src/components/OurGallary.js
@@ -1,8 +1,149 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+
+import project1 from '../static/projects/flip/1.jpg'
+import { Link } from 'react-router-dom';
 
 const OurGallary = () => {
   return (
-    <div>OurGallary</div>
+    <div>
+      <Navbar />
+      <div className="our_gallary_container">
+        <div className="our_gallary_title">
+          <h3 className='about_title'><i className='underlined'>Gall</i>ary</h3>
+        </div>
+        <div className="row">
+
+        <div className="col-md-3 col-sm-12 one_project_container">
+            <div className="project_card">
+              <div className="project_card_header">
+              </div>
+              <div className="project_card_body">
+                <div>
+                  <p className="project_card_title"><i className="underlined">First proj</i>ect</p>
+                </div>
+                <div>
+                  <i className="project_card_about">
+                    lorem ipsum dolor asd cfge pokfd ewf 
+                  </i>
+                </div>
+                <div>
+                  <Link className="project_card_button" to='/gallary/flip'>More</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-3 col-sm-12 one_project_container">
+            <div className="project_card">
+              <div className="project_card_header">
+              </div>
+              <div className="project_card_body">
+                <div>
+                  <p className="project_card_title"><i className="underlined">First proj</i>ect</p>
+                </div>
+                <div>
+                  <i className="project_card_about">
+                    lorem ipsum dolor asd cfge pokfd ewf 
+                  </i>
+                </div>
+                <div>
+                  <Link className="project_card_button" to='/gallary/flip'>More</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-3 col-sm-12 one_project_container">
+            <div className="project_card">
+              <div className="project_card_header">
+              </div>
+              <div className="project_card_body">
+                <div>
+                  <p className="project_card_title"><i className="underlined">First proj</i>ect</p>
+                </div>
+                <div>
+                  <i className="project_card_about">
+                    lorem ipsum dolor asd cfge pokfd ewf 
+                  </i>
+                </div>
+                <div>
+                  <Link className="project_card_button" to='/gallary/flip'>More</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-3 col-sm-12 one_project_container">
+            <div className="project_card">
+              <div className="project_card_header">
+              </div>
+              <div className="project_card_body">
+                <div>
+                  <p className="project_card_title"><i className="underlined">First proj</i>ect</p>
+                </div>
+                <div>
+                  <i className="project_card_about">
+                    lorem ipsum dolor asd cfge pokfd ewf 
+                  </i>
+                </div>
+                <div>
+                  <Link className="project_card_button" to='/gallary/flip'>More</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-3 col-sm-12 one_project_container">
+            <div className="project_card">
+              <div className="project_card_header">
+              </div>
+              <div className="project_card_body">
+                <div>
+                  <p className="project_card_title"><i className="underlined">First proj</i>ect</p>
+                </div>
+                <div>
+                  <i className="project_card_about">
+                    lorem ipsum dolor asd cfge pokfd ewf 
+                  </i>
+                </div>
+                <div>
+                  <Link className="project_card_button" to='/gallary/flip'>More</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-3 col-sm-12 one_project_container">
+            <div className="project_card">
+              <div className="project_card_header">
+              </div>
+              <div className="project_card_body">
+                <div>
+                  <p className="project_card_title"><i className="underlined">First proj</i>ect</p>
+                </div>
+                <div>
+                  <i className="project_card_about">
+                    lorem ipsum dolor asd cfge pokfd ewf 
+                  </i>
+                </div>
+                <div>
+                  <Link className="project_card_button" to='/gallary/flip'>More</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
